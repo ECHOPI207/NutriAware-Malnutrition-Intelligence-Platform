b74e3d75a6a4c2c77db408b45e87acd8ff70908c
Mohamed Mostafa <mohamed.mostafa.req@gmail.com>
2026-01-04 19:31:59 +0200

    Refactor: Remove unused dependencies and dead code

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./i18n/config";
-import "./data/seedArticles"; // Import for development seeding
 import App from "./App.tsx";
 import { AppWrapper } from "./components/common/PageMeta.tsx";
 import { LanguageProvider } from "./contexts/LanguageContext";
